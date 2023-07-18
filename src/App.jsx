08af1fc2ed35e72fde7fbcdba8b9aa0ece74738e
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-18 15:55:34 -0300

    Agrego dependencia router dom. Agrego browserRouter en main.jsx, creo componente Footer, lo agrego a la page, y devuelvo la page junto a las rutas en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
-
-
+import { Page } from "./pages/page";
 
 function App() {
   return (
-    <>    
+    <>
+      <Page>
+        <Routes>
+          <Route path="/" element={<h1>Prueba footer</h1>} />
+        </Routes>
+      </Page>
     </>
   );
 }
