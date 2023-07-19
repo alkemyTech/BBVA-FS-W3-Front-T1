5864db26b94ade0ba4c739874e56941233489c43
Evaristo00 <evaristogaston@gmail.com>
2023-07-19 16:45:24 -0300

    simulacion plazo fijo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, Outlet } from "react-router-dom";
 import "./App.css";
 import { Page } from "./pages/Page";
 import { LoadTransfer } from "./components/LoadTransfer/LoadTransfer"
+import { SimulatedFixedTerm } from "./components/SimulatedFixedTerm/SimulatedFixedTerm";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<h1>Prueba footer</h1>} />
           <Route path="transferencia" element={<><LoadTransfer /></>}>
           </Route>
+          <Route path="/plazo-fijo" element={<SimulatedFixedTerm/>}/>
         </Routes>
       </Page>
     </>
---
+++ src/components/TaskFixedInterface/TaskFixedInterface.jsx
@@ -0,0 +1,87 @@
+import { Grid, Typography, TextField, Alert, Button, Box } from '@mui/material'
+import React from 'react'
+import { useForm } from "react-hook-form";
+import "./TaskFixedInterface.css"
+
+export const TaskFixedInterface = ({ onSubmit, tipoCarga }) => {
+  const { register, handleSubmit, control, formState: { errors } } = useForm({
+    validateCriteriaMode: "all",
+    reValidateMode: "onChange",
+    mode: "onChange"
+  });
+
+  const validateAmount = (value) => {
+    if (!/^\d+$/.test(value)) {
+      return 'Ingresar solo numeros';
+    }
+    if (value <= 0) {
+      return 'Ingresar un importe positivo'
+    }
+    return true;
+  }
+
+  const validateDays = (value) => {
+    if (!/^\d+$/.test(value)) {
+      return 'Ingresar solo numeros';
+    }
+    if (value < 30) {
+      return 'Ingresar una cantidad de dias minima de 30'
+    }
+    return true;
+  }
+
+
+  return (
+
+    <Grid container
+      spacing={4}
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+      className='containerGrid'
+    >
+      {tipoCarga &&
+
+        <Grid item xs={12} >
+          <Typography 
+            color="#2BA0B5"
+            fontWeight="900"
+            fontSize="1.7em"
+            marginBottom="6vh"
+            borderBottom="2px solid ">
+            {tipoCarga}
+          </Typography>
+        </Grid>
+
+      }
+
+
+      <Grid item xs={12} >
+        <Typography className='informacion'> Ingersar cantidad a depositar:</Typography>
+      </Grid>
+
+      <Grid item xs={12} paddingBottom="5vh">
+        <TextField label="Importe a transferir" fullWidth
+          variant="standard"  {...register('amount', { validate: validateAmount })} />
+        {errors.amount && <Alert severity="error">{errors.amount.message}</Alert>}
+      </Grid>
+
+
+
+      <Grid item xs={12}>
+        <Typography className='informacion'> Ingersar cantidad de dias: </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField label="Importe a transferir" fullWidth
+          variant="standard"  {...register('days', { validate: validateDays })} />
+        {errors.days && <Alert severity="error">{errors.days.message}</Alert>}
+      </Grid>
+
+
+      <Grid item xs={12} >
+        <Button type="submit" variant="contained" className='boton'>
+          Siguiente
+        </Button>
+      </Grid>
+    </Grid>
+  )
+}
