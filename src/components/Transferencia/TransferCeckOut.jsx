19cde11947489ffca3009d4b25431ae9df408bdd
Evaristo00 <evaristogaston@gmail.com>
2023-07-21 14:37:46 -0300

    pruebas de conexion con el back

--- src/components/Transferencia/TransferCeckOut.jsx
+++ src/components/Transferencia/TransferCeckOut.jsx
@@ -51,34 +51,54 @@ export const TransferCeckOut = () => {
     handleNext();
   };
 
-  const ResumeSubmit = async () => {
-    console.log(dataAccount);
+  const ResumeSubmit = async (e) => {
+    e.preventDefault();
+    debugger
+    // console.log(dataAccount);
     const token = localStorage.getItem("token");
-    console.log(token);
-    const idAccount = 28;
 
-    const requestBody = {
-      id: idAccount,
-      amount: dataAccount.amount,
-    };
+    // const idAccount = 28;
+
+    // const requestBody = {
+    //   id: idAccount,
+    //   amount: "1",
+    // };
+
+    // console.log(requestBody);
 
     const config = {
       headers: { Authorization: `Bearer ${token}` },
     };
 
-    const apiUrl =
-      dataAccount.currency === "ARS"
-        ? "http://localhost:8080/transactions/sendArs"
-        : "http://localhost:8080/transactions/sendUsd";
+    // const apiUrl =
+    //   dataAccount.currency === "ARS"
+    //     ? "http://localhost:8080/transactions/sendArs"
+    //     : "http://localhost:8080/transactions/sendUsd";
 
-    axios
-      .post(apiUrl, requestBody, config)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    // console.log(apiUrl);
+    // axios.defaults.headers.common['Authorization']=`Bearer ${token}`
+
+    // console.log(axios.defaults.headers.common['Authorization']);
+    try {
+      const response = await axios.get("http://localhost:8080/accounts/balance",config);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+    // try {
+    //   const response = await axios.post(apiUrl, {id: 28,amount: "1"});
+    //   console.log(response);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+    // axios
+    //   .post(apiUrl, requestBody, config)
+    //   .then((response) => {
+    //     console.log(response.data);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
     // handleNext();
   };
 
