e9e9372910f2a0871aab22b9e66adf3cda5856af
DiegoPerez <diego_martin_180@hotmail.com>
2023-07-21 10:24:30 -0300

    arreglo import Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
 import { Route, Routes, Outlet } from "react-router-dom";
 import "./App.css";
-import { Login } from "./components/login/Login";
+import { Login } from "./components/Login/Login";
 import { Page } from "./pages/Page";
 import { LoadTransfer } from "./components/LoadTransfer/LoadTransfer";
 import { SimulatedFixedTerm } from "./components/FixedTerm/SimulatedFixedTerm/SimulatedFixedTerm";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
-  const user = useSelector((state)=>state.user.userName)
-  
+  const user = useSelector((state) => state.user.userName);
+
+  const handleUnload = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    localStorage.removeItem("nombre");
+  };
+
+  useEffect(() => {
+    window.onunload = handleUnload;
+    return () => {
+      window.onunload = null;
+    };
+  }, []);
+
   return (
     <>
       <Page>
         <Routes>
-          <Route
-            path="*"
-            element={<Login/>}
-          />
+          <Route path="*" element={<Login />} />
           {user && (
             <>
               <Route path="/home" element={<h1>Prueba Home</h1>} />
